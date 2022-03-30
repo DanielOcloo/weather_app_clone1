d29a8fd34d1259285ded7c754aa358797e2ea09e
DanielOcloo <83448178+DanielOcloo@users.noreply.github.com>
2022-03-30 12:24:20 +0000

    Revert "Removed Comma from line 14"
    This reverts commit 9fa7cbcab27a68d59b9805092bcf793dd6abf703.

--- lib/search.dart
+++ lib/search.dart
@@ -11,7 +11,11 @@ class MySearchPage extends StatefulWidget {
 }
 
 class _MySearchPageState extends State<MySearchPage> {
-  Widget _infoColumn(String title, String subtitle, {bool inverse = false}) {
+  Widget _infoColumn(
+    String title,
+    String subtitle, {
+    bool inverse = false,
+  }) {
     return Column(
       children: <Widget>[
         Text(
